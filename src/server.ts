c816ba5299e484849d11aaab0b2272fe9ee532f5
Xavier Vidal <xaviv@multiestetica.com>
2020-06-22 00:33:48 +0200

    Exercise done on server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -17,7 +17,6 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -26,17 +25,36 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get('/filteredimage', async (req, res) => {
+
+    // 1 Validate the image_url query
+    const imageUrl = req.query.image_url;
+    if (!imageUrl) {
+      return res.status(400).send("image_url parameter is required");
+    }
+
+    // 2 call filterImageFromURL(image_url) to filter the image
+    // TODO Check for error
+    const filteredImage = await filterImageFromURL(imageUrl);
+
+    // 3 send the resulting file in the response
+    res.sendFile(
+      filteredImage,
+      // 4 deletes any files on the server on finish of the response
+      () => deleteLocalFiles([filteredImage])
+    );
+  } );
 
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
